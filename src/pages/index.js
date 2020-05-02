254821d14b5d7c49bb38a4208ff33873fbbb0fa3
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-05-02 13:51:05 -0300

    PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
-    <ul>
-      <li>
-        <Link to="/" activeStyle={{ color: "red" }}>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about" activeStyle={{ color: "red" }}>
-          About
-        </Link>
-      </li>
-    </ul>
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
