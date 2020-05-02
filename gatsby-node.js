db7c6f31225bc3d65634e0eec115fd2cfb1e4b87
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-05-02 15:30:52 -0300

    graphql query with param + create pages with template

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 const { createFilePath } = require("gatsby-source-filesystem")
+const { resolve } = require("path")
 
 // To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -27,4 +28,34 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: `/${slug.slice(12)}`,
     })
   }
+}
+
+// To create the posts pages
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
