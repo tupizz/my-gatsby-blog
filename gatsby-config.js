9025407180f2eec64139f853c2ff6f32e9d36673
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-05-01 21:27:25 -0300

    Cleanup starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog de Tadeu Tupinambá`,
+    description: `Blog sobre tecnologias e desenvolvimento de software.`,
+    author: `@tupizz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
